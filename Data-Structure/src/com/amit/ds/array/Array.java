7ce3120cf7002c51a74a3d7ddf7c7d784f0449b5
amitranjan1289 <amitranjan1289@gmail.com>
2016-09-08 23:58:19 +0530

    Array program

---
+++ Data-Structure/src/com/amit/ds/array/Array.java
@@ -0,0 +1,62 @@
+package com.amit.ds.array;
+
+public class Array {
+	int[] array = null;
+	int i=0;
+	int j=5;
+	
+	public Array() {
+		array = new int[5];
+	}
+/**
+ * This method add the integer element.
+ * @param element
+ */
+	public void add(int element) {
+		if(size()<array.length){
+			array[i++]=element;
+			
+		}
+		else{
+			copyArray(element);
+		}
+	}
+	private void copyArray(int element){
+		int[] temp=new int[size()*2];
+		for(int i=0;i<size();i++){
+			temp[i]=array[i];
+		}
+		array=temp;
+		array[i++]=element;
+	}
+	
+	private int size(){		
+		return i;
+	}
+	
+	public void show(){
+		for(int i=0;i<size();i++){
+			System.out.println(array[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Array array=new Array();
+		array.add(10);
+		array.add(20);
+		array.add(30);
+		array.add(40);
+		array.add(50);
+		array.add(60);
+		array.add(70);
+		array.add(10);
+		array.add(20);
+		array.add(30);
+		array.add(40);
+		array.add(50);
+		array.add(60);
+		array.show();		
+   
+	}
+
+}
---
+++ Data-Structure/src/com/amit/ds/array/TestArray.java
@@ -0,0 +1,27 @@
+package com.amit.ds.array;
+
+public class TestArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Array array=new Array();
+		array.add(10);
+		array.add(20);
+		array.add(30);
+		array.add(40);
+		array.add(50);
+		array.add(60);
+		array.add(70);
+		array.add(10);
+		array.add(20);
+		array.add(30);
+		array.add(40);
+		array.add(50);
+		array.add(60);
+		array.show();		
+   
+	}
+}
+
